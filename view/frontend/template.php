312638580b3d533fdaaf2bb6507174697be95515
tristancien <76054522+tristancien@users.noreply.github.com>
2020-12-23 16:16:16 +0100

    ajout favicon

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html>
-
-    <!--    FACEBOOK    -->
-
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v8.0" nonce="56uyCiBA"></script>
-
-    <body>
-        <div class="alignement">
-            <head>
+<head>
                 <meta charset="utf-8" />
 
                 <!-- ############# FAVICON ############ -->
@@ -35,6 +27,14 @@
                 <title><?= $title ?></title>
                 <link href="public/cs/style.css" rel="stylesheet" /> 
             </head>
+
+    <!--    FACEBOOK    -->
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v8.0" nonce="56uyCiBA"></script>
+
+    <body>
+        <div class="alignement">
 
             <header class="banniere">
                 <a href="home.php"><img src="public/image/banner-me.jpg" alt="Banniere du site" /></a>
