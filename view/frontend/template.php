581b7bb2d8c76251f9669c32609b2fd1b39c156c
tristancien <76054522+tristancien@users.noreply.github.com>
2020-12-23 16:19:14 +0100

    test upgrade speed fb

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html>
+<!--    FACEBOOK    -->
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v8.0" nonce="56uyCiBA"></script>
+
 <head>
                 <meta charset="utf-8" />
 
@@ -28,11 +33,7 @@
                 <link href="public/cs/style.css" rel="stylesheet" /> 
             </head>
 
-    <!--    FACEBOOK    -->
-
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v8.0" nonce="56uyCiBA"></script>
-
+    
     <body>
         <div class="alignement">
 
