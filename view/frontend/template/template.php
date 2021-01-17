a0f9babaf3ca59d62672292cac2794b130502135
tristancien <76054522+tristancien@users.noreply.github.com>
2021-01-17 02:40:34 +0100

    Button collapse nav
    Ajout librairie bootstrap + mise en place du bouton collapse pour la navbar

--- view/frontend/template/template.php
+++ view/frontend/template/template.php
@@ -25,6 +25,14 @@
     <!-- ###################################### -->
     
     <title><?= $title ?></title>
+
+    <!-- AJOUT LIBRAIRIE POUR BOOTSTRAP POUR LE COLLAPSE -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" > 
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
     <link href="public/css/style.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"> <!-- Ajout des favicons --> 
 </head>
