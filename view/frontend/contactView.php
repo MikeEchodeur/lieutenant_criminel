6859ec618c0bcceb5caba7e2c137edb203b01f54
tristancien <76054522+tristancien@users.noreply.github.com>
2021-01-31 16:23:32 +0100

    modif contactview

--- view/frontend/contactView.php
+++ view/frontend/contactView.php
@@ -14,120 +14,39 @@
 	</p>
 </div>
 
-<script type="text/javascript">
-        $(function () {
-    
-    $('#contact-form').submit(function(e) {
-        e.preventDefault();
-        $('.comments').empty();
-        var postdata = $('#contact-form').serialize();
-        
-        $.ajax({
-            type: 'POST',
-            url: 'php/contact.php',
-            data: postdata,
-            dataType: 'json',
-            success: function(json) {
-                 
-                if(json.isSuccess) 
-                {
-                    $('#contact-form').append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>");
-                    $('#contact-form')[0].reset();
-                }
-                else
-                {
-                    $('#username + .comments').html(json.usernameError);
-                    $('#email + .comments').html(json.emailError);
-                    $('#website + .comments').html(json.websiteError);
-                    $('#phone + .comments').html(json.phoneError);
-                    $('#message_contact + .comments').html(json.message_contactError);
-                    $('#sujet + .comments').html(json.sujetError);
-                }                
-            }
-        });
-    });
-
-})
-
-</script>
-
 <form method="post" action="">
-		<p class="form_contact">
+	<p class="form_contact">
 
-			<label for="username" required><b>Nom ou surnom</b> (obligatoire)</label>
-			<input type="text" name="username" id="username" placeholder="Sgt Touneuf" required>
+		<label for="username" required><b>Nom ou surnom</b> (obligatoire)</label>
+		<input type="text" name="username" id="username" placeholder="Sgt Touneuf" required>
+		<p class="comments"></p>
 
-			<label for="email" required><b>E-mail</b> (obligatoire)</label>
-			<input type="email" name="email" id="email" placeholder="CuicuiJeSuisUnFruit@intradef.gouv.fr" required>
+		<label for="email" required><b>E-mail</b> (obligatoire)</label>
+		<input type="email" name="email" id="email" placeholder="CuicuiJeSuisUnFruit@intradef.gouv.fr" required>
+		<p class="comments"></p>
 
-			<label for="website"><b>Site Web</b></label>
-			<input type="url" name="website" id="website" placeholder="www.ISuckMyCDS.fr">
+		<label for="website"><b>Site Web</b></label>
+		<input type="url" name="website" id="website" placeholder="www.ISuckMyCDS.fr">
+		<p class="comments"></p>
 
-			<label for="sujet">Sujet (obligatoire)</label>                 
-	            <select name="sujet" id="sujet">
-	            <option value="">Quel est le sujet de votre message ?</option>
-	            <option value="Remerciement">Remerciement</option>
-	            <option value="Retex">Retex</option>
-	            <option value="Autre question">Autre</option>
-	            </select>
+		<label for="sujet">Sujet (obligatoire)</label>                 
+        <select name="sujet" id="sujet">
+        <option value="">Quel est le sujet de votre message ?</option>
+        <option value="Remerciement">Remerciement</option>
+        <option value="Retex">Retex</option>
+        <option value="Autre question">Autre</option>
+        </select>
+        <p class="comments"></p>
 
-			<label for="contact_comment" required><b>Commentaire</b> (obligatoire)</label>
-			<textarea name="contact_comment" id="contact_comment" placeholder="Mes respects Mike,
-			Trop mdr ce que tu fais. Je suis ton plus grand fan. Ahouuu la colo" required></textarea>
-			
-			<input type="submit" value="Envoyer">
-		</p>
-		</form>
+		<label for="contact_comment" required><b>Commentaire</b> (obligatoire)</label>
+		<textarea name="contact_comment" id="contact_comment" placeholder="Mes respects Mike,
+		Trop mdr ce que tu fais. Je suis ton plus grand fan. Ahouuu la colo" required></textarea>
+		<p class="comments"></p>
+		
+		<input type="submit" value="Envoyer">
+	</p>
+</form>
 
-
-<div class="row">
-   <div class="col-lg-10 col-lg-offset-1">
-        <form id="contact-form" method="post" action="" role="form">
-            <div class="row">
-                <div class="col-md-6">
-                    <label for="username">Nom <span class="blue">*</span></label>
-                    <input id="username" type="text" name="username" class="form-control" placeholder="Votre Nom">
-                    <p class="comments"></p>
-                </div>
-                <div class="col-md-6">
-                    <label for="email">Email <span class="blue">*</span></label>
-                    <input id="email" type="text" name="email" class="form-control" placeholder="Votre Email">
-                    <p class="comments"></p>
-                </div>
-                <div class="col-md-6">
-                    <label for="phone">Téléphone</label>
-                    <input id="phone" type="tel" name="phone" class="form-control" placeholder="Votre Téléphone">
-                    <p class="comments"></p>
-                </div>
-                <div class="col-md-6">
-                    <label for="website">Site Web</label>
-                    <input id="website" type="url" name="website" class="form-control" placeholder="www.ISuckMyCDS.fr">
-                    <p class="comments"></p>
-                </div>
-                <div class="col-md-12">
-                    <label for="sujet">Sujet <span class="blue">*</span></label>                 
-    	            <select name="sujet" id="sujet">
-    	            <option value="">Quel est le sujet de votre message ?</option>
-    	            <option value="Remerciement">Remerciement</option>
-    	            <option value="Retex">Retex</option>
-    	            <option value="Autre question">Autre</option>
-    	            </select>
-                </div>
-                <div class="col-md-12">
-                    <label for="message_contact">Message <span class="blue">*</span></label>
-                    <textarea id="message_contact" name="message_contact" class="form-control" placeholder="Votre Message" rows="4"></textarea>
-                    <p class="comments"></p>
-                </div>
-                <div class="col-md-12">
-                    <p class="blue"><strong>* Ces informations sont requises.</strong></p>
-                </div>
-                <div class="col-md-12">
-                    <input type="submit" class="button1" value="Envoyer">
-                </div>    
-            </div>
-        </form>
-    </div>
-</div>
 
 <?php $content = ob_get_clean(); ?>
 
