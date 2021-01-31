87fba0e305d535ee43dcded1aeda5605d5627370
tristancien <76054522+tristancien@users.noreply.github.com>
2021-01-31 16:04:39 +0100

    update ind

--- view/frontend/template/ind.php
+++ view/frontend/template/ind.php
@@ -13,41 +13,7 @@
     
     
     <body>
-        <script type="text/javascript">
-            $(function () {
-    
-    $('#contact-form').submit(function(e) {
-        e.preventDefault();
-        $('.comments').empty();
-        var postdata = $('#contact-form').serialize();
         
-        $.ajax({
-            type: 'POST',
-            url: 'php/contact.php',
-            data: postdata,
-            dataType: 'json',
-            success: function(json) {
-                 
-                if(json.isSuccess) 
-                {
-                    $('#contact-form').append("<p class='thank-you'>La réponse arrivera en même temps que ton colis au SMCAT</p>");
-                    $('#contact-form')[0].reset();
-                }
-                else
-                {
-                    $('#username + .comments').html(json.usernameError);
-                    $('#email + .comments').html(json.emailError);
-                    $('#website + .comments').html(json.websiteError);
-                    $('#phone + .comments').html(json.phoneError);
-                    $('#message_contact + .comments').html(json.message_contactError);
-                    $('#sujet + .comments').html(json.sujetError);
-                }                
-            }
-        });
-    });
-
-})
-        </script>
        <div class="container">
             <div class="divider"></div>
                             
