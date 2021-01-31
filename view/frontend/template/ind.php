d48a1505cb58fc8c07e91dc71d2cef14f5113b7c
tristancien <76054522+tristancien@users.noreply.github.com>
2021-01-31 15:53:23 +0100

    update ind

--- view/frontend/template/ind.php
+++ view/frontend/template/ind.php
@@ -7,8 +7,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="css/style.css">
+        <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
         <script src="js/script.js"></script>
     </head>
     
