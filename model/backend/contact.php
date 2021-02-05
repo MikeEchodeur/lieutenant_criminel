feea34ba8400ee6151e8ffacae22f92f587525aa
tristancien <76054522+tristancien@users.noreply.github.com>
2021-02-05 21:25:08 +0100

    .

--- model/backend/contact.php
+++ model/backend/contact.php
@@ -80,6 +80,7 @@
             $header = "MIME Version 1.0\r\n";
             $header .= "Content-type : text/html; charset=UTF-8\r\n";
             $headers .= "From: {$array['username']} <{$array['email']}>\r\nReply-To: {$array['email']}";
+            
             mail($emailTo, $emailSujet, $emailText, $headers);
         }
         
