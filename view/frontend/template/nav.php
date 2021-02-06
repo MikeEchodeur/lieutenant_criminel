a47499d8acfe22cb7aabff02ba8a6a656afb8f7e
tristancien <76054522+tristancien@users.noreply.github.com>
2021-02-06 20:35:53 +0100

    ajout picto linkedin

--- view/frontend/template/nav.php
+++ view/frontend/template/nav.php
@@ -32,6 +32,7 @@
     <div class="picto_sociaux"> <!-- CI DESSOUS CSS DU RULEVIEW POUR LA COULEUR ET LA TAILLE DES PICTOGRAMMES -->
         <a href="https://www.facebook.com/moitiedemilouf/" target="_blank" class="share-fb"><i class="fab fa-facebook"></i></a>
         <a href="https://www.instagram.com/lieutenant.criminel/" target="_blank" class="share-insta"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/groups/13938659/?fbclid=IwAR2YHH-HwKhTDpRNwn5RNrzqM671vZXcfQ3eb1nyWSOBhk2Q7i7MJeIGm44" target="_blank" class="share-linkedin"><i class="fab fa-linkedin"></i></a>
         <a href="youtube ???" class="share-youtube"><i class="fab fa-youtube"></i></a>
     </div>
 </nav>
