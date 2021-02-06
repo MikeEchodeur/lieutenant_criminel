28d53943f71ac02c04306f6f54df1a81fb9c3d78
tristancien <76054522+tristancien@users.noreply.github.com>
2021-02-06 01:24:36 +0100

    Form - ajout * pour le requis

--- view/frontend/contactView.php
+++ view/frontend/contactView.php
@@ -20,13 +20,13 @@
     <form id="contact-form" method="post" action="" role="form">
         <div class=" libele_double">
             <div class=" libele_double_inside">
-                <label for="username">Nom (obligatoire) :</label>
+                <label for="username">Nom<span class="red"> * </span>:</label>
                 <input id="username" type="text" name="username" class="form-control" placeholder="Comme ta bande patro Bitoss">
                 <p class="comments"></p>
             </div>
 
             <div class=" libele_double_inside">
-                <label for="email">Email (obligatoire) :</label>
+                <label for="email">Email<span class="red"> * </span>:</label>
                 <input id="email" type="text" name="email" class="form-control" placeholder="CuicuiJeSuisUnFruit@intradef.gouv.fr">
                 <p class="comments"></p>
             </div>
@@ -47,7 +47,7 @@
         </div>
 
         <div class="libele_simple">
-            <label for="sujet">Sujet (obligatoire) :</label>                 
+            <label for="sujet">Sujet<span class="red"> * </span>:</label>                 
             <select name="sujet" id="sujet">
             <option value="">Quel est le sujet de votre message ?</option>
             <option value="Remerciement">Remerciement</option>
@@ -59,13 +59,13 @@
         </div>
 
         <div class="libele_simple">
-            <label for="message_contact">Message (obligatoire) :</label>
+            <label for="message_contact">Message<span class="red"> * </span>:</label>
             <textarea id="message_contact" name="message_contact" class="form-control" placeholder="Trop marrant Mike Echo" rows="4"></textarea>
             <p class="comments"></p>
         </div>
 
         <div>
-            <p class="blue"><strong>* Ces informations sont requises.</strong></p>
+            <p><strong><span class="red">* </span>Ces informations sont requises.</strong></p>
         </div>
 
         <div class="div_send">
