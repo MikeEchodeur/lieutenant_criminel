0996e9b9f9c520031e8f2ce16649d462c87e0072
tristancien <76054522+tristancien@users.noreply.github.com>
2021-02-07 18:25:52 +0100

    custom form registration

--- view/frontend/public/inscriptionView.php
+++ view/frontend/public/inscriptionView.php
@@ -2,20 +2,23 @@
 
 <?php ob_start(); ?>
 
-<p>Inscrivez-vous :</p>
-	<form action="index.php?action=inscription" method="post">
-    <p>
-	    <label for="username"> Pseudo :</label> <input type="text" name="username" id="username" required> <br>
-	    <label for="password"> Mot de passe :</label> <input type="password" name="password" id="password" required/> <br>
-	    <label for="confirmPassword"> Confirmez le mot de passe :</label> <input type="password" name="confirmPassword" id="confirmPassword" required> <br>
-	    <label for="email"> Adresse Email :</label> <input type="email" name="email" id="email" required/> <br>
-	    <input type="submit" value="Inscription"/>
-		</br>
-    </p>
-	</form>
-
+<div class="mainAndFb">
+	<div class="registration">
+		<p style="text-decoration: underline">Inscrivez-vous :</p>
+		<form id="form_registration" action="index.php?action=inscription" method="post">
+		    <p>
+			    <label for="username"> Pseudo :</label> <input type="text" name="username" id="username" required>
+			    <label for="password"> Mot de passe :</label> <input type="password" name="password" id="password" required>
+			    <label for="confirmPassword"> Confirmez le mot de passe :</label> <input type="password" name="confirmPassword" id="confirmPassword" required>
+			    <label for="email"> Adresse Email :</label> <input type="email" name="email" id="email" required/>
+			    <div class="div_send">
+			    	<input type="submit" value="Inscription">
+				</div>
+		    </p>
+		</form>
+	</div>
 	<?php include("view/frontend/template/facebook.php"); ?>
-
+</div>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('view/frontend/template/template.php'); ?>
