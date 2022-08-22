ad9262a46505320a6803e087425563f9850b0269
Senshee <oooevolution@gmail.com>
2022-08-22 20:20:39 +0100

    Update template.php

--- view/frontend/template/template.php
+++ view/frontend/template/template.php
@@ -26,9 +26,9 @@
 
     <!-- ################## OPEN GRAPH pour reseau sociaux #################### -->
 
-    <meta property="og:image" content="<?= $image ?>" />
-    <meta property="og:image:secure_url" content="<?= $image ?>" />
-    <meta property="og:title" content="Lieutenant-Crimineltest"/>
+    <meta property="og:image" content="<?= $image?>" />
+    <meta property="og:image:secure_url" content="<?= $image?>" />
+    <meta property="og:title" content="<?= $title?>"/>
     <meta property="og:description" content="<?= $title?>"/>
     
     <!--<title><?= $title ?></title>-->
